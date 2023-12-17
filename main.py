3b1980de154a2db454de66d7906b39760e6ae139
Mike Kondrashov <kondrashovmischa@yandex.ru>
2023-12-17 14:45:15 +0300

    added eat

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Бим бим бам бам")
+import arcade
+
+
+class Window(arcade.Window):
+    def __init__(self):
+        super().__init__(1000, 800, "Ем чипсЕки")
+        self.bg = arcade.load_texture("env/chips.png")
+        self.z = 0
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_lrwh_rectangle_textured(0 - self.z, 0, window.width, window.height, self.bg)
+        arcade.draw_lrwh_rectangle_textured(window.width - self.z, 0, window.width, window.height, self.bg)
+        self.z += 5
+        if self.z > window.width:
+            self.z = 0
+
+
+window = Window()
+arcade.run()
